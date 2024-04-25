b8a9f014862b262df00335480f30bbd61db3c6ea
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-04-25 18:26:59 +0530

    implement map method

--- HigherOrderFunction.playground/Contents.swift
+++ HigherOrderFunction.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
+ 
+/*
+ Transforming all elements of an array requries a lot of code which includes
+ creating a new array, looping through the array and appending to it.
+ Swift arrays have a map method which was adopted from functional programming.
+*/
 
-var greeting = "Hello, playground"
+let nums = [1, 2, 3, 4]
+let squared = nums.map { element in element * element }
+
+/* This version has three main advantages
+ - it's shorter
+ - less room for error prone
+ - more importanlty it's clearer and all clutter has been removed
+ - once you see map you immediately know a function will be applied to every element
+   and return a new array of transformed element
+ - declaration of `squared` need to be `var` it can be `let` if appropriate
+ - type of the content of the new array can be infered from the function passed to map, `squared`
+   needs to be explicitly typed
+*/
+
+// Possbile implementation of `map`. In Swift it's an extension on `Sequence` protocol
+
+extension Array {
+    func map<T>(_ transform: (Element) -> T) -> [T] {
+        var result: [T] = []
+        result.reserveCapacity(count)
+        
+        for element in self {
+            result.append(transform(element))
+        }
+        
+        return result
+    }
+}
+
+/*
+ - `Element` is generic placeholder for whatever type the array contains.
+ -`T` is a new placeholder that represent the result of the element transformation.
+   It's known from the return type of the transformation closure
+*/
+
+/*
+ Really the signature of this method should be below indicating `map` will forward any error
+ the transformation function might throw to the caller
+*/
+
+extension Array {
+    func map<T>(_ transform: (Element) throws -> T) rethrows -> [T] { [] }
+}
+
+
+

efea171bce7339758eee42411978711f316dc69c
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-08 22:49:00 +0530

    understand merge method of dictionary to merge two dictionary. If they key are duplicated use any of the two values for the key

--- CollectionDataType.playground/Contents.swift
+++ CollectionDataType.playground/Contents.swift
@@ -2,19 +2,32 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-var dict = [1: "One", 2: "Two", 3: "Three"]
+var mainDict = [1: "One", 2: "Two", 3: "Three"]
 
-if let value = dict.removeValue(forKey: 1) {
+// Removing a value for the key
+if let value = mainDict.removeValue(forKey: 1) {
     print("\(value) removed from dict")
 } else {
     print("No value for the key")
 }
 
-if let previousValue = dict.updateValue("Five", forKey: 5) {
+mainDict[1] = nil
+
+// Updating a value for the key
+if let previousValue = mainDict.updateValue("Five", forKey: 5) {
     print("\(previousValue) was updated with Five")
 } else {
     print("New key value pair 5 and Five added")
 }
 
-dict[4] = "Four"
-print("Dictionary", dict)
+mainDict[4] = "Four"
+print("Dictionary", mainDict)
+
+// Merging two dictionary and if there is duplicate key, you can decide which dictionary value to be considered
+let otherDict = [2: "TWO"]
+let mergedDict = mainDict.merging(otherDict) { firstDictValue, secondDictValue in
+    // This closure is called only for duplicate key.
+    return secondDictValue
+}
+
+print(mergedDict)

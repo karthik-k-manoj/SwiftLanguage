a89d7a0faa5a9e8ccd030e93c884282b9edd2bf5
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-08 22:23:25 +0530

    understand dictionary update and remove method

--- CollectionDataType.playground/Contents.swift
+++ CollectionDataType.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+var dict = [1: "One", 2: "Two", 3: "Three"]
+
+if let value = dict.removeValue(forKey: 1) {
+    print("\(value) removed from dict")
+} else {
+    print("No value for the key")
+}
+
+if let previousValue = dict.updateValue("Five", forKey: 5) {
+    print("\(previousValue) was updated with Five")
+} else {
+    print("New key value pair 5 and Five added")
+}
+
+dict[4] = "Four"
+print("Dictionary", dict)

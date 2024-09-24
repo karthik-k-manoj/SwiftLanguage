ecd48fa0ebb3f5fc1c5a19948e0575f870829462
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-09-24 10:01:02 +0530

    optional

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -90,6 +90,12 @@ do {
                                     // return a result that will be also an optional
 }
 
+extension String {
+    func optionalUpperCased() -> String? {
+        nil
+    }
+}
+
 do {
     // as the name implies optional chaining you are not limited to a single method call after the
     // question mark. You can chain calls on optional values
@@ -99,4 +105,46 @@ do {
     // the above might suprise as a method called with optional chaining returns an optional
     // but we are not using `?` after uppercased(). Why?. This is because optional chaining is
     // a flattening operation
+    
+    let lower2 = str?.optionalUpperCased()?.lowercased()
+}
+
+extension Int {
+    var half: Int? {
+        guard self < -1  || self > 1 else { return nil }
+        
+        return self / 2
+    }
+}
+
+do {
+    var result = 20.half?.half?.half
+    // note 20 is not wrapped inside an optional and that is not wrapped inside anothe optional and again such as Int???
+    // instead it is Int?
+}
+
+do {
+    // optional chaining applies to subscripts
+    let dictOfArrays = ["nine": [1,2,3]]
+    let result = dictOfArrays["nine"]?[1]
+}
+
+do {
+    // optional chaining applies to to call optional function
+    let dictOfFunctions: [String: (Int, Int) -> Int] = [
+        "add": (+),
+        "minus": (-)
+    ]
+    
+    let result = dictOfFunctions["add"]?(1,1)
+}
+
+// optional vars (not lets) are implicity initialized to nil
+// only exception Swift makes from it's strict explicit initialization policy
+
+class OptionalInit {
+    var didChange2: (() -> String)?
+    // var didChange2: Optional<() -> String>
+    // compiler would ask to init `didChange2`. Default initialization
+    // only works for short hand notation `?`
 }
